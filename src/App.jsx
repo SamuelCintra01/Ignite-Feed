69563d1d368f6ed91bf35678a31438698390ad59
Samuel Cintra <128873760+SamuelCintra01@users.noreply.github.com>
2023-07-13 13:41:54 -0300

    Componente_ Header

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+
+import "./global.css";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <Post author="Samuel" content="Futuro Programador Front End" />
+    </div>
+  );
+}
