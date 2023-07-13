51b13da50041cd87037cad495838296d755a7921
Samuel Cintra <128873760+SamuelCintra01@users.noreply.github.com>
2023-07-13 19:40:07 -0300

    Componente_Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,22 @@ import { Post } from "./Post";
 import { Header } from "./components/Header";
 
 import "./global.css";
+import styles from "./app.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post author="Samuel" content="Futuro Programador Front End" />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Samuel Cintra"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores cum quia minima a repudiandae blanditiis."
+          />
+        </main>
+      </div>
     </div>
   );
 }
