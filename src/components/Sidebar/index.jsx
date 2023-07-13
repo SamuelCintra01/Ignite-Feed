2709229913da9bb7c06b26641e64f7931c36c6ab
Samuel Cintra <128873760+SamuelCintra01@users.noreply.github.com>
2023-07-13 20:16:27 -0300

    Finalizado Componente Sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,15 +1,28 @@
+import { PencilLine } from "phosphor-react";
+
 import styles from "./index.module.css";
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
-      <img src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w500&q=50" />
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1534972195531-d756b9bfa9f2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w500&q=50"
+      />
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://github.com/SamuelCintra01.png"
+          alt=""
+        />
         <strong>Samuel Cintra</strong>
         <span>Web Developer</span>
       </div>
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
