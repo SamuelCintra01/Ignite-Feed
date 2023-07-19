167a884b3434a2afbbe1b8cc05330ade4daee341
Samuel Cintra <128873760+SamuelCintra01@users.noreply.github.com>
2023-07-19 14:17:13 -0300

    Componente_Comment

---
+++ src/components/Comment/index.jsx
@@ -0,0 +1,40 @@
+import { ThumbsDown, ThumbsUp, Trash } from "phosphor-react";
+
+import styles from "./index.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/SamuelCintra01.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Samuel Cintra</strong>
+              <time
+                title="15 de Julho ás 10:28h"
+                dateTime="2023-07-15 10:28:30"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar Comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! </p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
