4aefec9f717668d25f277cf2b6ea7ed56084bbc0
Samuel Cintra <128873760+SamuelCintra01@users.noreply.github.com>
2023-07-19 14:11:32 -0300

    Componente_Post

---
+++ src/components/Post/index.jsx
@@ -0,0 +1,48 @@
+import { Comment } from "../Comment";
+
+import styles from "./index.module.css";
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img src="https://github.com/SamuelCintra01.png" />
+          <div className={styles.authorInfo}>
+            <strong>Samuel Cintra</strong>
+            <span>Dev Front-end</span>
+          </div>
+        </div>
+        <time title="15 de Julho ás 10:28h" dateTime="2023-07-15 10:28:30">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href="">jane.design/doctorcare</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea placeholder="Deixe um comentário" />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  );
+}
