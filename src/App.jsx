7e9303b3021fff13ebb688497ae8a7f56966e132
Samuel Cintra <128873760+SamuelCintra01@users.noreply.github.com>
2023-07-19 14:18:26 -0300

    Aplicação atualizada com os novos componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Post } from "./Post";
+import { Post } from "./components/Post";
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./app.module.css";
 
 import "./global.css";
-import styles from "./app.module.css";
-import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
@@ -12,6 +13,10 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
+          <Post
+            author="Samuel Cintra"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores cum quia minima a repudiandae blanditiis."
+          />
           <Post
             author="Samuel Cintra"
             content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores cum quia minima a repudiandae blanditiis."
